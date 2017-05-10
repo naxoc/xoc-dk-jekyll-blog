a78e9121a7fb5774620db1050b25f95c51795029
Camilla Krag Jensen <ckj@information.dk>
2017-05-10 22:25:52 +0200

    Don't delete older stuff

--- publish.sh
+++ publish.sh
@@ -11,6 +11,5 @@ cp -r older _site/
 rsync -avz --delete _site/ almond:/var/www/html/xoc.dk
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
 
-rm -r _site/older
 # Remove the local "deployment" file.
 rm _site/VERSION.txt
