f8fd3ed0c03f7f71c36d5921175b379416cdeeaf
Camilla Krag Jensen <ckj@information.dk>
2017-05-10 22:26:01 +0200

    Found my old image folder!

---
+++ assets/images/bots.php
@@ -0,0 +1,41 @@
+<?	
+	include("../functions/func.inc.php");
+	HTMLheader();
+	leftSide();
+?>
+		<div id="middle">
+			<p>
+				A bot would be a little gif with background transparency and a format of 70 * 70 pixles.
+			</p>
+			<p>
+				ajax
+				<img src="bots/ajax.gif" alt="the clean bot">
+				kitty
+				<img src="bots/kitty.gif" alt="the kitty bot">
+				mouse
+				<img src="bots/mouse.gif" alt="The mouse bot">
+				puter
+				<img src="bots/puter.gif" alt="the IT bot">
+				tank
+				<img src="bots/tank.gif" alt="the army bot">
+				cloud
+				<img src="bots/cloud.gif" alt="the celestial proud bot">
+				bear
+				<img src="bots/bear.gif" alt="the ursus bot">
+				hexapod
+				<img src="bots/hexapod.gif" alt="the alien bot">
+				rabbit
+				<img src="bots/rabbit.gif" alt="the rodent bot">
+				square
+				<img src="bots/square.gif" alt="the square bot">
+				tipi
+				<img src="bots/tipi.gif" alt="the apache bot">
+				tv
+				<img src="bots/tv.gif" alt="the telebotty">
+			</p>
+		</div>
+		
+<?	rightSide();
+?>      
+	</body>
+</html>
